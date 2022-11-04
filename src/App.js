144909b8e391599821503d6083a9d2f56e21e338
ShakSphere <110363828+ShakSphere@users.noreply.github.com>
2022-11-05 05:04:52 +1100

    Needed to change name to use npm

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { questions } from "./questions";
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const [currentQuestion, setCurrentQuestion] = React.useState(0);
+  const [score, setScore] = React.useState(0);
+  const [showScore, setShowScore] = React.useState(false);
+
+  const handleClick = (isCorrect) => {
+    if (isCorrect) {
+      setScore(score + 1);
+    }
+
+    const nextQuestion = currentQuestion + 1;
+    if (nextQuestion < questions.length) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      setShowScore(true);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {showScore ? (
+        <section className="showScore-section">
+          Your score is {score} out of {questions.length}        
+        </section>
+    
+      ) : (
+        <>
+          <section className="header">
+          OptimalArabic.
+          </section>
+          <section className="h1">   
+          اختبار صغير 1: الدرس الأول - دورة الإعراب للمبتدئين 
+          </section>
+          <section className="footer">   
+            <b>بسم الله الرحمن الرحيم</b> 
+            <br></br>
+            <b>أيها الطالب !</b> 
+            <br></br>
+                1.     استعن بالله
+                <br></br>
+                2. اعلم أنّ    كل سؤال يحتسب علامة (1) واحدة 
+          </section>
+
+          <section className="question-section">
+            <h1>
+              سؤال {currentQuestion + 1}/{questions.length}
+            </h1>
+            <p>{questions[currentQuestion].questionText}</p>
+          </section>
+
+          <section className="answer-section">
+            {questions[currentQuestion].answerOptions.map((item) => (
+              <button onClick={() => handleClick(item.isCorrect)}>
+                {item.answerText}
+              </button>
+            ))}
+          </section>
+        </>
+      )}
     </div>
   );
 }
-
-export default App;
---
+++ src/questions.js
@@ -0,0 +1,50 @@
+export const questions = [
+  {
+    questionText: " ذهبَ الطَالب إلى السوق. ما إعراب 'ذهب' ",
+    answerOptions: [
+      { answerText: "فعل ماضي مبني على الفتحة لا محل لها من الإعراب", isCorrect: true },
+      { answerText: "فعل مضارع مبني على الفتحة لا محل له من الإعراب", isCorrect: false },
+      { answerText: "فعل مضارع مرفوع وعلامة رفعه الضمة الظاهرة على آخره", isCorrect: false },
+      { answerText: "فعل مضارع مرفوع وعلامة رفعه الضمة الظاهرة على آخره", isCorrect: false },
+    ],
+  },
+  {
+    questionText: " ذهبَ الطالب إلى السوق. ما إعراب 'السوق' ",
+    answerOptions: [
+      { answerText: "فاعل مرفوع وعلامة رفعه الضمة الظاهرة على آخره", isCorrect: false },
+      { answerText: "مفعول به منصوب وعلامة نصبه الفتحة الظاهرة على آخره", isCorrect: false },
+      { answerText: "اسم مجرور مجرور وعلامة جره الكسرة الظاهرة على آخره", isCorrect: true },
+
+    ],
+  },
+  {
+    questionText: "أكل التفاحة مخلص. ما إعراب 'مخلص'؟",
+    answerOptions: [
+      { answerText: "فاعل مرفوع وعلامة رفعه الضمة الظاهرة على آخره", isCorrect: true },
+      { answerText: "مفعول به منصوب وعلامة نصبه الفتحة الظاهرة على آخره", isCorrect: false },
+      { answerText: "اسم مجرور مجرور وعلامة جره الكسرة الظاهرة على آخره", isCorrect: false },
+  
+    ],
+  },
+  {
+    questionText: "لنْ يجلس الطفلُ. إعراب لَنْ: حرف نصب مبني على السكون لا محل له من الإعراب",
+    answerOptions: [
+      { answerText: "صحيح", isCorrect: true },
+      { answerText: "خطأ", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "لم يلعبِ الطالبُ. إعراب يلعب: فعل مضارع مجرور وعلامة جره الكسرة الظاهرة على آخره",
+    answerOptions: [
+      { answerText: "صحيح", isCorrect: false },
+      { answerText: "خطأ", isCorrect: true },
+    ],
+  },
+  {
+    questionText: "يرى المعلمُ المديرَ. إعراب رأى: فعل مضارع مرفوع وعلامة رفعه الضمة الظاهرة على آخره",
+    answerOptions: [
+      { answerText: "صحيح", isCorrect: false },
+      { answerText: "خطأ", isCorrect: true },
+    ],
+  },
+];
